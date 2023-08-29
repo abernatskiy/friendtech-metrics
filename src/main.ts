6da95b87a50f821c7af51d24b1a84383c4b9ba4f
abernatskiy <abernatskiy@toha.li>
2023-08-29 13:21:18 +0900

    Populating the VolumeByBlock entities
    	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import {TypeormDatabase} from '@subsquid/typeorm-store'
-import {Trade} from './model'
+import {Trade, VolumeByBlock} from './model'
 import {processor, FRIENDTECH_CONTRACT} from './processor'
 import * as friendTechAbi from './abi/friendTechAbi'
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
     const trades: Trade[] = []
+    const volumes: VolumeByBlock[] = []
 
     for (let block of ctx.blocks) {
+        let totalEthAmount = 0n
+
         for (let log of block.logs) {
             if (log.address === FRIENDTECH_CONTRACT &&
                 log.topics[0] === friendTechAbi.events.Trade.topic) {
@@ -21,6 +24,7 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
                     subjectEthAmount,
                     supply,
                 } = friendTechAbi.events.Trade.decode(log)
+
                 trades.push(new Trade({
                     id: log.id,
                     block: block.header.height,
@@ -34,9 +38,18 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
                     supply,
                     txnHash: log.transactionHash,
                 }))
+
+                totalEthAmount += ethAmount
             }
         }
+
+        volumes.push(new VolumeByBlock({
+            id: `${block.header.height}`,
+            block: block.header.height,
+            totalEthAmount
+        }))
     }
 
     await ctx.store.upsert(trades)
+    await ctx.store.upsert(volumes)
 })
